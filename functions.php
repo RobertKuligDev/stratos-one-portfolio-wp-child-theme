e342ddcc984c66928726b138be8740497d99f82c
RobertKuligDev <git@robertkulig-dev.eu>
2026-03-17 21:25:53 +0100

    feat(theme): add “Stratos One” custom WP theme section (pattern + styling)

--- functions.php
+++ functions.php
@@ -28,6 +28,12 @@ add_action('wp_enqueue_scripts', function() {
         [],
         filemtime(get_stylesheet_directory() . '/assets/css/project-content.css')
     );
+    wp_enqueue_style(
+        'stratos-one-custom-wp-theme',
+        get_stylesheet_directory_uri() . '/assets/css/custom-wp-theme.css',
+        [],
+        filemtime( get_stylesheet_directory() . '/assets/css/custom-wp-theme.css' )
+    );
 },);
 
 add_action( 'init', function() {
@@ -61,6 +67,14 @@ add_action( 'init', function() {
             'title'      => __( 'Project Content', 'stratos-one' ),
             'categories' => [ 'stratos-one' ],
             'content'    => file_get_contents( get_stylesheet_directory() . '/patterns/project-content.html' ),
+        ]
+    );
+    register_block_pattern(
+        'stratos-one-portfolio/custom-wp-theme',
+        [
+            'title'      => __( 'Custom WP Theme (Stratos One)', 'stratos-one' ),
+            'categories' => [ 'stratos-one' ],
+            'content'    => file_get_contents( get_stylesheet_directory() . '/patterns/custom-wp-theme.html' ),
         ]
     );
 });
